742f9dce1c7a3eec574cd913177f0bf9ba63c8e7
YuudaiHiiro <jamroack@gmail.com>
2023-12-17 17:43:17 +0200

    bootstrap, navbar, login/logout function

--- crmx/website/views.py
+++ crmx/website/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    # Check login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Authenthicate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Authentication successful!")
+            return redirect ('home')
+        else:
+            messages.success(request, "Authentication failed, try again.")
+            return redirect ('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Logout successful!")
+    return redirect ('home')
